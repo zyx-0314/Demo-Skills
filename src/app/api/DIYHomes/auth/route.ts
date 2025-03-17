726ad7216b86049173ef76b2ece61671c1ff6be9
zyx-0314 <ramirezian037@gmail.com>
2025-03-18 00:30:26 +0800

    fix(auth): rename firebaseAdminApp variable for consistency

--- src/app/api/DIYHomes/auth/route.ts
+++ src/app/api/DIYHomes/auth/route.ts
@@ -2,17 +2,17 @@ import admin, { ServiceAccount } from "firebase-admin";
 import { getApps, getApp, initializeApp, App } from "firebase-admin/app";
 
 // Ensure Firebase Admin is initialized only once
-let firebaseAdminApp: App;
+let FirebaseAdminApp: App;
 
 if (!getApps().length) {
-  firebaseAdminApp = initializeApp({
+  FirebaseAdminApp = initializeApp({
     credential: admin.credential.cert(
       JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string) as ServiceAccount
     ),
   });
 } else {
-  firebaseAdminApp = getApp();
+  FirebaseAdminApp = getApp();
 }
 
-export const auth = admin.auth(firebaseAdminApp);
-export default firebaseAdminApp;
+export const auth = admin.auth(FirebaseAdminApp);
+export default FirebaseAdminApp;
